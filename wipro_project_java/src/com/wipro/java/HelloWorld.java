5aa64d91201d77587de15f668ae0197792e23581
Mahankali Mohana Subrahmanya Sai Ram <mmssairam45@gmail.com>
2025-02-01 12:35:12 +0530

    committing to dev

--- wipro_project_java/src/com/wipro/java/HelloWorld.java
+++ wipro_project_java/src/com/wipro/java/HelloWorld.java
@@ -21,6 +21,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("my first project, ");
+    System.out.println("sai ram");
 	}
 
 }
